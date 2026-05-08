d1705474235effbd1e4b90f61085435ff67d06f7
Yash Adke <yashadke2003@gmail.com>
2026-05-09 01:21:30 +0530

    fix authContext

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,21 +1,32 @@
 import axios from "axios";
-import React, { createContext, useContext, useEffect, useState } from "react";
+import React, {
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-  // 🔥 reusable function
+  // reusable function
   const fetchUser = async () => {
     try {
       const res = await axios.get(
         "https://lost-n-found-govn.onrender.com/api/user/getuser",
-        { withCredentials: true }
+        {
+          withCredentials: true,
+        }
       );
+
       setUser(res.data.user);
     } catch (err) {
       setUser(null);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -24,7 +35,14 @@ export const AuthProvider = ({ children }) => {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user, setUser, fetchUser }}>
+    <AuthContext.Provider
+      value={{
+        user,
+        setUser,
+        fetchUser,
+        loading,
+      }}
+    >
       {children}
     </AuthContext.Provider>
   );
