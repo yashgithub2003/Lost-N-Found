64fc4a909a4c1ebe6473e7aaca69a1a7ebb4192b
Yash Adke <yashadke2003@gmail.com>
2026-05-09 01:31:16 +0530

    set protected route

--- frontend/src/components/ProtectedRoutes.jsx
+++ frontend/src/components/ProtectedRoutes.jsx
@@ -1,16 +1,22 @@
-import { useAuth } from '@/context/AuthContext'
-import React, { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom';
+import { useAuth } from "@/context/AuthContext";
+import React from "react";
+import { Navigate } from "react-router-dom";
 
-const ProtectedRoutes = ({children}) => {
-    const { user } = useAuth();
-    const navigate = useNavigate();
-    useEffect(() => {
-        if(!user){
-            navigate('/login');
-        }
-    },[])
-  return <>{children}</>
-}
+const ProtectedRoutes = ({ children }) => {
+  const { user, loading } = useAuth();
 
-export default ProtectedRoutes
+  // wait for auth check
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  // if not logged in
+  if (!user) {
+    return <Navigate to="/login" />;
+  }
+
+  // authenticated
+  return <>{children}</>;
+};
+
+export default ProtectedRoutes;
