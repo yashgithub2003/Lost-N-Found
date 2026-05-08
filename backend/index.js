276bc78915f8a00320580b9b24a6e7c68e24ec0a
Yash Sanjay Adke <yash@Mac.lan>
2026-05-08 23:58:53 +0530

    update cors

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 const corsOptions = {
-  origin: "https://lost-n-found-99wa09udv-yash-adkes-projects.vercel.app",
+  origin: "https://lost-n-found-9y4kmht5d-yash-adkes-projects.vercel.app",
   credentials: true,
 };
 app.use(cors(corsOptions));
