d96c056223deb31151953e0bebacdbf4cc64233a
Yash Sanjay Adke <yash@Mac.lan>
2026-05-08 23:02:14 +0530

    for vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import path from "path"
 
 export default defineConfig({
   plugins: [react()],
+  base : process.env.VITE_BASE_PATH || "/Lost-N-Found/tree/main/frontend",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
